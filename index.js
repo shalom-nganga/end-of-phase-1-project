07d1017a6145cbf282b3634b570e669f4cf550c4
shalom-nganga <minnomacharia@gmail.com>
2023-10-26 15:07:50 +0300

    made changes to the index.js file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// First, create a function to fetch the data from the API
+// create a function to fetch the data from the API
 
 const fetchData = async () => {
     const response = await fetch("https://thronesapi.com/api/v2/Characters");
@@ -7,13 +7,13 @@ const fetchData = async () => {
     return data;
   };
   
-  // Next, create a function to display the data on the user interface
+
   const displayData = (data) => {
     const charactersContainer = document.querySelector("#characters-container");
-    // Clear the characters container before adding new data
+   
     charactersContainer.innerHTML = "";
   
-    // Iterate over the data and create a new element for each character
+  
     data.forEach((character) => {
       const characterElement = document.createElement("div");
        characterElement.classList.add("character");
@@ -28,30 +28,29 @@ const fetchData = async () => {
         });
       </script>
       `
-      // Add the character's image to the element
+     
       const imageElement = document.createElement("img");  //likebutton.style.color= "red"
       imageElement.src = character.imageUrl;
       characterElement.appendChild(imageElement);
   
-      // Add the character's name to the element
+    
       const nameElement = document.createElement("h3");
       nameElement.textContent = character.fullName
       characterElement.appendChild(nameElement);
   
-      // Add the character's title to the element
+      
       const titleElement = document.createElement("p");
       titleElement.textContent = character.title;
       characterElement.appendChild(titleElement);
   
-      // Add the character's family to the element
       const familyElement = document.createElement("p");
       familyElement.textContent = character.family;
       characterElement.appendChild(familyElement);
   
-      // Add the character element to the characters container
+     
       charactersContainer.appendChild(characterElement);
     });
   };
   
-  // Finally, call the fetchData() function and then the displayData() function
+  
   fetchData().then((data) => displayData(data));
